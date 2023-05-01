294d9de3ac9a83e11603fd2dbfdcb10934e5873d
wentokay <101902546+wentokay@users.noreply.github.com>
2023-05-01 15:12:38 +0100

    bs58

---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { encode, decode } from 'bs58';
+	import { Buffer } from 'buffer';
+
+	let toEncode = '';
+	let encoded = '';
+	$: try {
+		encoded = encode(Buffer.from(JSON.parse(toEncode)));
+	} catch (err) {
+		encoded = '';
+	}
+
+	let toDecode = '';
+	let decoded = '';
+	$: try {
+		decoded = decode(toDecode).toString();
+	} catch (err) {
+		decoded = '';
+	}
+</script>
+
+<h1>Base58 Encode</h1>
+<textarea bind:value={toEncode} placeholder="[128,0,100,...]" />
+<textarea readonly bind:value={encoded} placeholder="output" />
+
+<h1>Base58 Decode</h1>
+<textarea bind:value={toDecode} placeholder="Abc123..." />
+<textarea readonly bind:value={decoded} placeholder="output" />
+
+<style>
+	textarea {
+		width: 300px;
+		height: 300px;
+	}
+</style>
